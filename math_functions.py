95c100c89c4a8bb133301e9cf759e56156a2b389
Joshua Anders <joshua.anders@tn.techstarter.de>
2024-12-17 15:21:50 +0100

    Create math_functions.py

---
+++ math_functions.py
@@ -0,0 +1,22 @@
+# km to miles
+def km_to_miles(km):
+    zahl1 = float(km)
+    return zahl1 * float(0.621371)
+
+
+# miles to km
+def miles_to_km(m):
+    zahl1 = float(m)
+    return zahl1 / float(0.621371)
+
+
+# celsius to fahrenheit
+def celsius_to_fahrenheit(c):
+    zahl1 = float(c)
+    return zahl1 * 1.8 + 32
+
+
+# fahrenheit to celsius
+def fahrenheit_to_celsius(f):
+    zahl1 = float(f)
+    return (zahl1 - 32) * (5 / 9)
