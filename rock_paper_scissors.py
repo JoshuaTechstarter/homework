7fef60605ebc2717ea2da070874c133a2f0ba495
Joshua Anders <joshua.anders@tn.techstarter.de>
2024-12-18 17:40:24 +0100

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,32 +3,28 @@ import random
 
 
 while True:
-
     print("Lass uns eine Runde Schere-Stein-Papier spielen!")
     player_action = input("Wähle: (Schere, Stein, Papier): ")
     possible_actions = ["Schere", "Stein", "Papier"]
     com_action = random.choice(possible_actions)
     print(f"\nDeine Wahl ist {player_action}, COM Wahl ist {com_action}.\n")
 
+    # untentschieden
     if player_action == com_action:
         print(f"Beide Spieler haben {player_action} gewählt. Es ist unentschieden!")
-    elif player_action == "Stein":
-        if com_action == "Schere":
-            print("Stein schlägt Schere! Du hast gewonnen!")
-        else:
-            print("Papier schlägt Stein! Du hast verloren.")
-    elif player_action == "Papier":
-        if com_action == "Stein":
-            print("Papier schlägt Stein! Du hast gewonnen!")
-        else:
-            print("Schere schlägt Papier! Du hast verloren.")
-    elif player_action == "Schere":
-        if com_action == "Papier":
-            print("Schere schlägt Papier! Du hast gewonnen!")
-        else:
-            print("Stein schlägt Schere! Du hast verloren.")
+
+    elif (
+        (player_action == "Stein" and com_action == "Schere")
+        or (player_action == "Papier" and com_action == "Stein")
+        or (player_action == "Schere" and com_action == "Stein")
+    ):
+        print("Diese Runde geht an dich!")
+    else:
+        print("HHAHAHA LOSER")
 
     play_again = input("Noch eine Runde?(Y/N): ")
     if play_again.lower() != "y":
-
         break
+    else:
+        print("Bitte versuche es nochmal!")
+        continue
