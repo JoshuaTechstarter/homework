e9ca6d97b4ab8ab614c6fe117106f4984ca1de84
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-01-08 13:37:01 +0100

    Update 2025_01_07_JSON.py

--- 2025_01_07_JSON.py
+++ 2025_01_07_JSON.py
@@ -2,19 +2,19 @@ import requests
 
 
 def wetter_abfragen():
-    ort = input("Bitte geben Sie einen Ort ein: ")
+    city_name = input("Bitte geben Sie einen Ort ein: ")
 
-    url = f"https://wttr.in/{ort}?format=j1"
+    url = f"https://wttr.in/{city_name}?format=j1"
     response = requests.get(url)
     daten = response.json()
 
     temperatur = daten["current_condition"][0]["temp_C"]
-    gefuehlte_temperatur = daten["current_condition"][0]["FeelsLikeC"]
+    feels_like = daten["current_condition"][0]["FeelsLikeC"]
     area_name = daten["nearest_area"][0]["areaName"][0]["value"]
 
     print(f"\nWetterinformationen von der Wetterstation aus {area_name}:")
     print(f"Aktuelle Temperatur: {temperatur}°C")
-    print(f"Gefühlte Temperatur: {gefuehlte_temperatur}°C")
+    print(f"Gefühlte Temperatur: {feels_like}°C")
     print(f"")
 
 
