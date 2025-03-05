ff708d4a286b840da3e5ad7ca8a90346101d0a38
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-03-05 15:08:35 +0100

    Update signUp.js

---
+++ 2025_03_05_Login/signUp.js
@@ -0,0 +1,23 @@
+function onSignup(){
+    const userEmail = document.getElementById("emailti").value ;
+    const userPassword = document.getElementById("passwordti").value ;
+    const userPasswordAgain = document.getElementById("repeatPasswordti").value ;
+    const userFirstName = document.getElementById("firstName").value ;
+    const userLastName = document.getElementById("lastName").value ;
+
+    if (userEmail && userPassword && userPasswordAgain && userFirstName && userLastName !== undefined){
+        if (userEmail.indexOf("@") !== -1) {
+        } else {alert("Gib eine richtige E-Mail ein.")}
+        if (userPassword === userPasswordAgain){  
+            alert(`Nutzer erfolgreich angelegt\n E-Mail: ${userEmail}\n Passwort ${userPassword}\n First Name ${userFirstName}\n Last Name ${userLastName}`
+            ) 
+        } else {alert("Das Passwort stimmt nicht überein")}
+    } else { 
+        alert("Bitte fülle alle Felder aus")
+     } 
+    
+
+
+
+    
+}
