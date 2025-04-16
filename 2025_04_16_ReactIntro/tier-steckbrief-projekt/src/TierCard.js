3d63996723be242527ac2c601cbafb91363a5734
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-04-16 14:37:06 +0200

    Create TierCard.js

---
+++ 2025_04_16_ReactIntro/tier-steckbrief-projekt/src/TierCard.js
@@ -0,0 +1,13 @@
+import './TierCard.css';
+
+function TierCard({ name, art, krankheit }) {
+    return (
+        <div className="tier-card">
+            <h2>{name}</h2>
+            <p><strong>Art: </strong>{art}</p>
+            <p><strong>Krankheit: </strong>{krankheit}</p>
+        </div>
+    );
+}
+
+export default TierCard;
