8207958f936efd5d49c845d3bded1185c7db29ca
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-04-24 17:17:54 +0200

    Update App.js

--- 2025_04_22_ReactRouting/router-exercise/src/pages/App.js
+++ 2025_04_22_ReactRouting/router-exercise/src/pages/App.js
@@ -1,11 +1,11 @@
 import '../styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Patientenliste from './Patienteliste'
-import FAQ from './FAQ'
 import Pokemon from './Pokemon'
 import Kontakt from './contact'
 import Header from '../components/Header'
 import Home from './Home'
+import Pokedex from './Pokedex';
 
 
 function App() {
@@ -14,11 +14,11 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="patienten" element={<Patientenliste></Patientenliste>}>Unsere Patieren</Route>
-        <Route path="FAQ" element={<FAQ></FAQ>}>FAQ</Route>
-        <Route path="pokemon" element={<Pokemon></Pokemon>}>About us</Route>
-        <Route path="contact" element={<Kontakt></Kontakt>}>Kontakt</Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/patienten" element={<Patientenliste />}>Unsere Patieren</Route>
+        <Route path="/pokedex/:id" element={<Pokedex />} />
+        <Route path="/pokemon" element={<Pokemon />}>About us</Route>
+        <Route path="/contact" element={<Kontakt />}>Kontakt</Route>
       </Routes>
 
     </Router>
